1a2606ceaf03178297addcca6f49a69a8cec13b6
ElectricZebra <nicholasregoli@gmail.com>
2019-07-14 12:08:46 -0700

    new student form added (no js yet)

--- src/Nav.js
+++ src/Nav.js
@@ -11,6 +11,32 @@ const _Nav = ({ students, schools }) => {
       <Link to='/students'>Students ({students.length})</Link>
       <Link to='/most-popular'>Most Popular School</Link>
       <Link to='/highest-gpa'>School with Highest GPA</Link>
+      <form>
+        <label>
+          First Name:
+          <input type='text' />
+        </label>
+        <label>
+          Last Name:
+          <input type='text' />
+        </label>
+        <label>
+          Email:
+          <input type='email' />
+        </label>
+        <label>
+          GPA:
+          <input type='number' min='0' max='5' step='.01' />
+        </label>
+        <label>
+          Enrolled at:
+          <select>
+            <option value='hi'>hi</option>
+            { schools.map(school => <option key={school.id} value={school.name}>{school.name}</option>)}
+          </select>
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
     </div>
   )
 }
