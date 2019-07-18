2aeee5044652ac93f085e617dab067e7d13fd61d
ElectricZebra <nicholasregoli@gmail.com>
2019-07-18 10:05:23 -0700

    change student school in progress

--- src/Student.js
+++ src/Student.js
@@ -3,11 +3,22 @@ import { connect } from 'react-redux';
 import { destroy } from './store';
 
 const _Student = ({ student, schools, deleteStudent })=> {
+  console.log(student)
   return <div>
     <h4>{student.firstName} {student.lastName}</h4>
     <p>{ student.email }</p>
     <p>GPA: { student.gpa }</p>
-    <div>School: </div>
+    <p>School: { student.school.name }</p>
+    <form>
+      <label>Select School:
+        <select value={student.school.name}>
+          {schools.map(school => (
+            <option key={school.id} value={school.name}>{school.name}
+            </option>
+          ))}
+        </select>
+      </label>
+    </form>
     <button className={student.id} onClick={ deleteStudent }>Delete</button>
   </div>
 }
