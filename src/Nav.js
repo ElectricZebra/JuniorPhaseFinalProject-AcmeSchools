bcd12ea063101327b797c5f8203589273b565a54
ElectricZebra <nicholasregoli@gmail.com>
2019-07-17 18:19:33 -0700

    most pop link corrected

--- src/Nav.js
+++ src/Nav.js
@@ -3,14 +3,15 @@ import { connect } from 'react-redux';
 import React from 'react';
 import  CreateStudent  from './CreateStudent';
 
-const _Nav = ({ students, schools, popName, popVal }) => {
+const _Nav = ({ students, schools, popName, popVal, popId }) => {
+  console.log("popId", popId)
   return (
     <div>
       <h1>Acme Schools</h1>
       <Link to='/'>Home</Link>
       <Link to='/schools'>Schools ({schools.length})</Link>
       <Link to='/students'>Students ({students.length})</Link>
-      <Link to='/most-popular'>Most Popular { popName } ({ popVal })</Link>
+      <Link to={`/schools/${popId}`}>Most Popular { popName } ({ popVal })</Link>
       <Link to='/highest-gpa'>School with Highest GPA</Link>
       <CreateStudent />
     </div>
@@ -33,26 +34,29 @@ const mapStateToProps = ({ students, schools }) => {
     return acc
   }
 
-  let popName = '';
-  let popVal = 0;
+  let popName;
+  let popVal;
+  let popId = '';
 
   if (Object.keys(tally()).length) {
     const tallyObj = tally();
-    const popId = Object.keys(tallyObj).reduce((a, val)=> tallyObj[a] > tallyObj[val] ? a : val)
-    popVal = tallyObj[popId];
+    const id = Object.keys(tallyObj).reduce((a, val)=> tallyObj[a] > tallyObj[val] ? a : val)
+    popId = id;
+    popVal = tallyObj[id];
     schools.map(school => {
       for (let key in school) {
-        if (school.id === popId) {
+        if (school.id === id) {
           popName = school.name;
         }
       }
     })
   }
 
-
+console.log( popId)
   return {
     popName,
     popVal,
+    popId,
     students,
     schools
   }
